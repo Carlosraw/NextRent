01d63e103dfdc68f3344b15c8db627300853de27
Carlaris <carlaris@bancolombia.com.co>
2025-10-05 02:06:09 -0500

    Primer commit - proyecto NextRent

---
+++ src/main/java/org/nextrent/NextRentApplication.java
@@ -0,0 +1,13 @@
+package org.nextrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nextrent/NextRentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nextrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
